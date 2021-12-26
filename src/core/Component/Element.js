9ea3db76f6f20639b305ebf8ddef5e9497c187b1
Ha the hien <hathehien12a2@gmail.com>
2021-12-26 20:20:48 +0700

    divide form into elements, get attributes element

--- src/core/Component/Element.js
+++ src/core/Component/Element.js
@@ -1,5 +1,43 @@
 import Component from './Component';
 
-class Element extends Component {}
+class Element extends Component {
+    constructor(options, validator, element) {
+        this.options = options;
+        this.validator = validator;
+        this.attributes = null;
+
+        // get all attributes in DOM of element and value of this element
+        if (element)
+        {
+            var attributes = {}; 
+            $.each( element.attributes, function( index, attr ) {
+                // get name of element
+                if (attr.name === "name"){
+                    this.name = attr.value;
+                    return; // continue
+                }
+                // get id of element
+                if (attr.name === "id"){
+                    this.id = attr.value;
+                    return; // continue
+                }
+                // get type of element
+                if (attr.name === "type"){
+                    this.type = attr.value;
+                    return; // continue
+                }
+                // else add this attr to atributes
+                attributes[ attr.name ] = attr.value;
+            } ); 
+            this.attributes = attributes;
+        }
+
+        // need decorator method after if attribute == method name
+    }
+
+    validate(){
+
+    }
+}
 
 export default Element;
--- src/core/Component/Form.js
+++ src/core/Component/Form.js
@@ -1,9 +1,17 @@
 import Component from './Component';
+import Element from './Element';
 
 class Form extends Component {
-  constructor(options, validator) {
+  constructor(options, validator, form) {
     super(options, validator);
     this.elements = [];
+
+    // divide form into element
+    if (form.is( "form" )) {
+      for (let i = 0; i < form.length; i++) {
+        elements.push(Element(options, validator, form[i]));
+      }
+    }
   }
 
   retrieve() {
