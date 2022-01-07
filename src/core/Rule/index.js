5e4c1a62bff2be234e50e796ad4c12871e7c0945
Ha the hien <hathehien12a2@gmail.com>
2022-01-07 17:46:13 +0700

    update date message

--- src/core/Rule/index.js
+++ src/core/Rule/index.js
@@ -3,7 +3,7 @@ import validator from '../Validator/ValidatorMap.js';
 
 const rules = [
   new Rule('alphaonly', validator.alphaonly, 'Only alphabets are allowed.'),
-  new Rule('date', validator.date, 'The value must be date type 2015-03-25T14:48:00 or 2015/03/25 or 2015-03-25 or 25 Mar 2015'),
+  new Rule('date', validator.date, 'The value must be date type yyyy/mm/dd'),
   new Rule('require', validator.require, "The value must have value"),
   new Rule('email', validator.email, 'The value must be email'),
   new Rule('post', validator.require, 'The value must be post code type 12345 or 12345-1234 or 12345-12345 or 123456 or 123456-123456'),
